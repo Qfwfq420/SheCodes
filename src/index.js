fe5f7169981bb043ba81621a8985c065da98da23
Qfwfq420 <mahtabzarrinpashneh@gmail.com>
2023-01-18 16:58:38 +0330

    added wind speed and fixed current weather description

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ function getTempLoc(response) {
   temp.innerHTML = Math.round(response.data.main.temp);
   let name = document.querySelector(".city");
   name.innerHTML = response.data.name;
+  let logo = document.querySelector(".current_logo");
+  let logoUrl = `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`;
+  logo.setAttribute(`src`, logoUrl);
+  let wind = document.querySelector(".wind-speed");
+  let desc = document.querySelector(".weather-description");
+  wind.innerHTML = response.data.wind.speed;
+  desc.innerHTML = response.data.weather[0].description;
 }
 
 function getTemp(response) {
@@ -13,6 +20,13 @@ function getTemp(response) {
   let logo = document.querySelector(".current_logo");
   let logoUrl = `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`;
   logo.setAttribute(`src`, logoUrl);
+  let wind = document.querySelector(".wind-speed");
+  let desc = document.querySelector(".weather-description");
+  windValue = response.data.wind.speed;
+  console.log(windValue);
+  console.log(response.data.weather[0].description);
+  wind.innerHTML = windValue;
+  desc.innerHTML = response.data.weather[0].description;
 }
 
 function basedOnLoc(position) {
@@ -42,7 +56,6 @@ function changeTemp(event) {
   let unit = link.innerHTML;
   let num = document.querySelector("#num");
   let temp = Number(num.innerHTML);
-  console.log(num);
   if (unit === "C") {
     num.innerHTML = Math.round((temp * 9) / 5 + 32);
     link.innerHTML = "F";
@@ -51,6 +64,15 @@ function changeTemp(event) {
     link.innerHTML = "C";
   }
 }
+
+function def(city) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(getTemp);
+}
+
+def(`Tehran`);
 
 let time = document.querySelector(".date");
 let date = new Date();
